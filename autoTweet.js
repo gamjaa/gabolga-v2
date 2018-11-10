03a03c9ad5bfcba5a30af23433f1bf683b61c025
정경원 <mail@gamjaa.com>
2018-11-11 00:03:21 +0900

    자동 트윗을 공개된 트윗에 대한 장소만 올리도록 수정

--- autoTweet.js
+++ autoTweet.js
@@ -8,7 +8,16 @@ const telegramSend = require('./routes/telegram');
 
 const excute = async () => {
     const [rows] = await db.query('SELECT tweet_id, name, road_address, address FROM tweet');
-    const {tweet_id, name, road_address, address} = rows[_.random(rows.length - 1, false)];
+
+    const getRandomIndex = async () => {
+        const i = _.random(rows.length - 1, false);
+        return await T.get('statuses/oembed', {
+            id: rows[i].tweet_id
+        }).then(() => i).catch(() => Promise.resolve(getRandomIndex()));
+    };
+
+    const i = await getRandomIndex();
+    const {tweet_id, name, road_address, address} = rows[i];
 
     const status = `${moment.format('M월 D일')}, 오늘의 #가볼가 할 만한 곳\n${name}\n${road_address || address}\nhttps://gabolga.gamjaa.com/tweet/${tweet_id}`;
     await T.post('statuses/update', {
