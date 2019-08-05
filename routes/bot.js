d9bf2047c0b4084828c13e74539c714f4ea1920d
정경원 <mail@gamjaa.com>
2019-08-05 21:16:09 +0900

    오류 수정

--- routes/bot.js
+++ routes/bot.js
@@ -41,81 +41,54 @@ router.post('/', wrapAsync(async (req, res, next) => {
     if (header !== headerCheck) {
         return res.status(403).send();
     }
-
+    
     const gabolgaAsync = async (url, eventObject) => {
         if (!gabolgaRegex.test(url)) {
             return res.status(200).send();
         }
         
-        const userId = req.body.favorite_events[0].user.id_str;
-        const id = gabolgaRegex.exec(url)[1];
+        const userId = eventObject.user.id_str;
+        const tweetId = gabolgaRegex.exec(url)[1];
         const [users] = await db.query('SELECT user_id FROM users WHERE user_id=?', [userId]);
         if (!users.length) {
             await db.query('INSERT IGNORE INTO my_map (user_id, tweet_id) VALUES (?, ?)',
-                [userId, id]);
+                [userId, tweetId]);
             return res.status(200).send();
         }
 
-        const [myMaps] = await db.query('SELECT tweet_id FROM my_map WHERE user_id=? AND tweet_id=?', [userId, id]);
+        const [myMaps] = await db.query('SELECT tweet_id FROM my_map WHERE user_id=? AND tweet_id=?', [userId, tweetId]);
         if (myMaps.length) {
             return res.status(200).send();
         }
 
         await db.query('INSERT INTO my_map (user_id, tweet_id) VALUES (?, ?)',
-            [userId, id]);
+            [userId, tweetId]);
 
-        const [rows] = await db.query('SELECT name FROM tweet WHERE tweet_id=?', [id]);
+        const [rows] = await db.query('SELECT name FROM tweet WHERE tweet_id=?', [tweetId]);
         await sendDM(userId, {
-            text: `${req.body.favorite_events[0].user.name} 님의 지도에 '${rows[0].name}'이(가) 기록되었습니다. 확인해보세요!`,
+            text: `${eventObject.user.name} 님의 지도에 '${rows[0].name}'이(가) 기록되었습니다. 확인해보세요!`,
             ctas: [
                 {
                     type: 'web_url',
                     label: '내 지도 보기',
-                    url: `https://gabolga.gamjaa.com/my/map?tweet_id=${id}`
+                    url: `https://gabolga.gamjaa.com/my/map?tweet_id=${tweetId}`
                 }
             ]
         }).catch((err) => Promise.resolve(console.log(err.stack)));
         
         return res.status(200).send();
+    };
+
+    // Favorite
+    if (_.get(req.body, 'favorite_events[0].favorited_status.user.id_str') === '903176813517479936') {
+        const url = _.get(req.body, 'favorite_events[0].favorited_status.entities.urls[0].expanded_url');
+        return await gabolgaAsync(url, req.body.favorite_events[0]);
     }
 
     // RT
     if (_.get(req.body, 'tweet_create_events[0].retweeted_status.user.id_str') === '903176813517479936') {
         const url = _.get(req.body, 'tweet_create_events[0].retweeted_status.entities.urls[0].expanded_url');
-        if (!gabolgaRegex.test(url)) {
-            return res.status(200).send();
-        }
-        
-        const userId = req.body.tweet_create_events[0].user.id_str;
-        const id = gabolgaRegex.exec(url)[1];
-        const [users] = await db.query('SELECT user_id FROM users WHERE user_id=?', [userId]);
-        if (!users.length) {
-            await db.query('INSERT IGNORE INTO my_map (user_id, tweet_id) VALUES (?, ?)',
-                [userId, id]);
-            return res.status(200).send();
-        }
-
-        const [myMaps] = await db.query('SELECT tweet_id FROM my_map WHERE user_id=? AND tweet_id=?', [userId, id]);
-        if (myMaps.length) {
-            return res.status(200).send();
-        }
-
-        await db.query('INSERT INTO my_map (user_id, tweet_id) VALUES (?, ?)',
-            [userId, id]);
-
-        const [rows] = await db.query('SELECT name FROM tweet WHERE tweet_id=?', [id]);
-        await sendDM(userId, {
-            text: `${req.body.tweet_create_events[0].user.name} 님의 지도에 '${rows[0].name}'이(가) 기록되었습니다. 확인해보세요!`,
-            ctas: [
-                {
-                    type: 'web_url',
-                    label: '내 지도 보기',
-                    url: `https://gabolga.gamjaa.com/my/map?tweet_id=${id}`
-                }
-            ]
-        }).catch(() => Promise.resolve());
-
-        return res.status(200).send();
+        return await gabolgaAsync(url, req.body.tweet_create_events[0]);
     }
 
     // DM
@@ -123,49 +96,9 @@ router.post('/', wrapAsync(async (req, res, next) => {
         req.body.direct_message_events[0].message_create.sender_id !== '903176813517479936') {
         const senderId = req.body.direct_message_events[0].message_create.sender_id;
         const text = req.body.direct_message_events[0].message_create.message_data.text;
-        const noSpaceText = text.replace(/\s/g, '');
 
         await db.query('INSERT INTO dm (dm_id, user_id, text) VALUES (?, ?, ?)', 
             [req.body.direct_message_events[0].id, senderId, text]);
-
-        if (['거부', '거절', '달지', '안달', '안다', '불허', '원치', '허락하지'].includes(noSpaceText)) {
-            await Mention.setPermission(senderId, true);
-            await sendDM(senderId, {
-                text: '답변해주셔서 감사합니다. 거부 처리 완료되었습니다.\n혹시 생각이 바뀌신다면 언제든지 "멘션허용"이라고 보내주시면 됩니다. 감사합니다.',
-                quick_reply: {
-                    type: 'options',
-                    options: [
-                        {
-                            label: '멘션허용',
-                            description: '답글이 달리는 것을 허용합니다',
-                            metadata: senderId
-                        }
-                    ]
-                }
-            });
-
-            return res.status(200).send();
-        }
-
-        if (['허용', '허락', '다셔도', '달아도', '괜찮', '상관없'].includes(noSpaceText)) {
-            await Mention.setPermission(senderId, false);
-            await sendDM(senderId, {
-                text: `허락해주셔서 감사합니다, ${name} 님!\n혹시 생각이 바뀌신다면 언제든지 "멘션거부"라고 보내주시면 됩니다. 불편하게 느끼시지 않도록 노력하겠습니다.\n좋은 하루 보내시길 바랍니다.`,
-                quick_reply: {
-                    type: 'options',
-                    options: [
-                        {
-                            label: '멘션거부',
-                            description: '답글이 달리는 것을 거부합니다',
-                            metadata: senderId
-                        }
-                    ]
-                }
-            });
-            await Mention.sendMentionInQue(senderId);
-
-            return res.status(200).send();
-        }
 
         const quickReply = _.get(req.body, 'direct_message_events[0].message_create.message_data.quick_reply_response.metadata');
         if (quickReply) {
@@ -284,61 +217,106 @@ router.post('/', wrapAsync(async (req, res, next) => {
         if (!id) {
             const [user] = await db.query('SELECT search_tweet_id FROM users WHERE user_id=?', [senderId]);
 
-            if (!user.length || !user[0].search_tweet_id) {
-                const q = text.replace(/[.~!*()'"]/g, '');
-                const searchBtnLabel = `가볼가 검색: ${q}`;
+            if (user.length && user[0].search_tweet_id) {
+                // 검색어
+                const {items} = await localSearch(text);
+                const places = items.map(item => {
+                    const {name, address, road_address, phone, mapx, mapy} = item;
+                    return {
+                        label: name,
+                        description: `${address || road_address} / ${phone}`,
+                        metadata: JSON.stringify({name, address, road_address, phone, mapx, mapy})
+                    };
+                });
+                await db.query('INSERT INTO search_log(tweet_id, keyword) VALUES (?, ?)', [user[0].search_tweet_id, text]);
                 await sendDM(senderId, {
-                    text: `문의 및 건의사항은 멘션이나 @_gamjaa(DM)로 보내주세요.\n사용법이 궁금하시거나, 가볼가에서 '${q}'(으)로 검색하시려면 아래 버튼을 눌러주세요! 감사합니다.`,
-                    ctas: [
-                        {
-                            type: 'web_url',
-                            label: '가볼가 사용법',
-                            url: 'https://gabolga.gamjaa.com/guide'
-                        },
-                        {
-                            type: 'web_url',
-                            label: (searchBtnLabel.length > 36 ? `${searchBtnLabel.slice(0, 33)}...` : searchBtnLabel),
-                            url: `https://gabolga.gamjaa.com/search?q=${encodeURIComponent(q)}`
-                        },
-                        {
-                            type: 'web_url',
-                            label: '@_gamjaa로 DM 보내기',
-                            url: 'https://twitter.com/messages/compose?recipient_id=62192325'
-                        },
-                    ]
+                    text: places.length 
+                        ? `'${text}'(으)로 검색된 장소들입니다. 원하는 장소를 선택해주세요. 다른 키워드로 다시 검색할 수도 있습니다.\n장소 목록이 안 보인다면, 화면 하단의 ☰ 버튼을 눌러주세요.`
+                        : `'${text}'에 대한 검색 결과가 없습니다. 다른 키워드로 다시 검색해주세요. 지역명(구, 동)을 빼고 장소명만으로도 검색해보세요!`,
+                    quick_reply: {
+                        type: 'options',
+                        options: [
+                            ...places,
+                            {
+                                label: '등록 취소',
+                                description: '등록 과정을 취소합니다',
+                                metadata: `{"isClose": "${req.body.direct_message_events[0].id}"}`
+                            }
+                        ]
+                    }
                 });
 
-                await telegramSend([`@${req.body.users[senderId].screen_name}`, text], true);
+                return res.status(200).send();
+            }
+
+            const noSpaceText = text.replace(/\s/g, '');
+                
+            if (/거부|거절|달지|안달|안다|불허|원치|원하지|허락하지/.test(noSpaceText)) {
+                // 거부 메시지
+                await Mention.setPermission(senderId, true);
+                await sendDM(senderId, {
+                    text: '답변해주셔서 감사합니다. 거부 처리 완료되었습니다.\n혹시 생각이 바뀌신다면 언제든지 "멘션허용"이라고 보내주시면 됩니다. 감사합니다.',
+                    quick_reply: {
+                        type: 'options',
+                        options: [
+                            {
+                                label: '멘션허용',
+                                description: '답글이 달리는 것을 허용합니다',
+                                metadata: senderId
+                            }
+                        ]
+                    }
+                });
+    
+                return res.status(200).send();
+            }
+    
+            if (/허용|허락|다셔도|달아도|괜찮|상관없/.test(noSpaceText)) {
+                // 허용 메시지
+                await Mention.setPermission(senderId, false);
+                await sendDM(senderId, {
+                    text: `허락해주셔서 감사합니다, ${req.body.users[senderId].name} 님!\n혹시 생각이 바뀌신다면 언제든지 "멘션거부"라고 보내주시면 됩니다. 불편하게 느끼시지 않도록 노력하겠습니다.\n좋은 하루 보내시길 바랍니다.`,
+                    quick_reply: {
+                        type: 'options',
+                        options: [
+                            {
+                                label: '멘션거부',
+                                description: '답글이 달리는 것을 거부합니다',
+                                metadata: senderId
+                            }
+                        ]
+                    }
+                });
+                await Mention.sendMentionInQue(senderId);
     
                 return res.status(200).send();
             }
 
-            const {items} = await localSearch(text);
-            const places = items.map(item => {
-                const {name, address, road_address, phone, mapx, mapy} = item;
-                return {
-                    label: name,
-                    description: `${address || road_address} / ${phone}`,
-                    metadata: JSON.stringify({name, address, road_address, phone, mapx, mapy})
-                };
-            });
-            await db.query('INSERT INTO search_log(tweet_id, keyword) VALUES (?, ?)', [user[0].search_tweet_id, text]);
+            // 그 외
+            const q = text.replace(/[.~!*()'"]/g, '');
+            const searchBtnLabel = `가볼가 검색: ${q}`;
             await sendDM(senderId, {
-                text: places.length 
-                    ? `'${text}'(으)로 검색된 장소들입니다. 원하는 장소를 선택해주세요. 다른 키워드로 다시 검색할 수도 있습니다.\n장소 목록이 안 보인다면, 화면 하단의 ☰ 버튼을 눌러주세요.`
-                    : `'${text}'에 대한 검색 결과가 없습니다. 다른 키워드로 다시 검색해주세요. 지역명(구, 동)을 빼고 장소명만으로도 검색해보세요!`,
-                quick_reply: {
-                    type: 'options',
-                    options: [
-                        ...places,
-                        {
-                            label: '등록 취소',
-                            description: '등록 과정을 취소합니다',
-                            metadata: `{"isClose": "${req.body.direct_message_events[0].id}"}`
-                        }
-                    ]
-                }
+                text: `문의 및 건의사항은 멘션이나 @_gamjaa(DM)로 보내주세요.\n사용법이 궁금하시거나, 가볼가에서 '${q}'(으)로 검색하시려면 아래 버튼을 눌러주세요! 감사합니다.`,
+                ctas: [
+                    {
+                        type: 'web_url',
+                        label: '가볼가 사용법',
+                        url: 'https://gabolga.gamjaa.com/guide'
+                    },
+                    {
+                        type: 'web_url',
+                        label: (searchBtnLabel.length > 36 ? `${searchBtnLabel.slice(0, 33)}...` : searchBtnLabel),
+                        url: `https://gabolga.gamjaa.com/search?q=${encodeURIComponent(q)}`
+                    },
+                    {
+                        type: 'web_url',
+                        label: '@_gamjaa로 DM 보내기',
+                        url: 'https://twitter.com/messages/compose?recipient_id=62192325'
+                    },
+                ]
             });
+
+            await telegramSend([`@${req.body.users[senderId].screen_name}`, text], true);
 
             return res.status(200).send();
         }
@@ -408,7 +386,7 @@ router.post('/', wrapAsync(async (req, res, next) => {
         const senderId = eventObject.user.id_str;
         const noSpaceText = eventObject.text.replace(/\s/g, '');
 
-        if (['거부', '거절', '달지', '안달', '안다', '불허', '원치', '원하지', '허락하지'].includes(noSpaceText)) {
+        if (/거부|거절|달지|안달|안다|불허|원치|원하지|허락하지/.test(noSpaceText)) {
             // 거절 멘션
             await Mention.setPermission(senderId, true);
             await postT.post('statuses/update', {
@@ -419,7 +397,7 @@ router.post('/', wrapAsync(async (req, res, next) => {
             return res.status(200).send();
         }
 
-        if (['허용', '허락', '다셔도', '달아도', '괜찮', '상관없'].includes(noSpaceText)) {
+        if (/허용|허락|다셔도|달아도|괜찮|상관없/.test(noSpaceText)) {
             // 허용 멘션
             await Mention.setPermission(senderId, false);
             await postT.post('statuses/update', {
